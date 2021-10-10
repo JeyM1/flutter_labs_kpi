9ad5af3080bff963141e6660b0e4eba7a5d677fe
Denys Afendikov <denys.afendikov@axel.org>
2021-10-10 23:10:12 +0300

    Added Drawer.

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,15 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   int _selectedIndex = 0;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void _openEndDrawer() {
+    _scaffoldKey.currentState!.openEndDrawer();
+  }
+
+  void _closeEndDrawer() {
+    Navigator.of(context).pop();
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -70,6 +79,7 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         leading: SvgPicture.asset(
           '../resources/images/YouTube-Icon-Full-Color-Logo.wine.svg',
@@ -115,10 +125,7 @@ class _AppState extends State<App> {
             },
           ),
           IconButton(
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('In development')));
-            },
+            onPressed: _openEndDrawer,
             icon: CircleAvatar(
               radius: 20,
               backgroundImage:
@@ -246,6 +253,33 @@ class _AppState extends State<App> {
           onTap: _onItemTapped,
         showUnselectedLabels: true,
       ),
+      endDrawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              child: Text('My Account'),
+            ),
+            ListTile(
+              title: const Text('My channel'),
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('In development')));
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Something other...'),
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('In development')));
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      endDrawerEnableOpenDragGesture: false,
       body: _indexToPageMap.elementAt(_selectedIndex),
     );
   }
